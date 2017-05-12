fbe48a6143e3a9e2b58af30683ca5efe9429f59a
Donovan Hiland <donovan.hiland@gmail.com>
2017-05-12 13:53:24 -0600

    removed and replaced deprecated React.PropTypes

--- lib/src/components/Form/Form.jsx
+++ lib/src/components/Form/Form.jsx
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Base from './../Base/Base';
 import rules from './../../rules';
 
